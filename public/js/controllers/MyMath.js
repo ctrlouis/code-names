764943dcde9c788c4a578108902c08ec0ef3a631
ctrlouis <louis.bty57@gmail.com>
2019-12-28 18:51:31 +0100

    create function who return random integer number

--- public/js/controllers/MyMath.js
+++ public/js/controllers/MyMath.js
@@ -3,6 +3,11 @@
 
 class MyMath {
 
+    // return random number between 0 and "max"
+    static getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
 }
 
 export default MyMath;
