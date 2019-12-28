a92743bf8332f9a22197624b0c854b71cf737d0b
ctrlouis <louis.bty57@gmail.com>
2019-12-28 18:51:53 +0100

    create function who return array of random integer (unique or not)

--- public/js/controllers/MyMath.js
+++ public/js/controllers/MyMath.js
@@ -3,6 +3,22 @@
 
 class MyMath {
 
+    static getRandomIntArray(size, max, unique =false) {
+        let rng = [];
+        let number;
+
+        while (rng.length < size) {
+            number = MyMath.getRandomInt(max);
+            if (unique) {
+                if ( !rng.includes(number) ) rng.push(number);
+            } else {
+                rng.push(number);
+            }
+        }
+
+        return rng;
+    }
+
     // return random number between 0 and "max"
     static getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
