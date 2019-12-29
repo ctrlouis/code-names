26660b97bc5b2bcc76bbec7f5ddf6762cf612448
ctrlouis <louis.bty57@gmail.com>
2019-12-29 16:27:37 +0100

    split function correctly

--- public/js/controllers/Cards.js
+++ public/js/controllers/Cards.js
@@ -8,7 +8,9 @@ class Card {
 
     // return array randomly fill with value of "initalWords" array
     static distribute() {
-        return Card.getRandomCards(initalWords, 25);
+        let cards =  Card.getRandomCards(initalWords, 25);
+        cards = Card.makeUsable(cards);
+        return cards;
     }
 
     // create array of size "number" randomly take in array "cards"
@@ -20,7 +22,7 @@ class Card {
             randomCards.push(cards[index]);
         });
 
-        return Card.makeUsable(randomCards);
+        return randomCards;
     }
 
     static makeUsable(words) {
